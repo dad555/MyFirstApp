25cc95973c44d31541b3417f19daa983fcb675f3
dad555 <Art101985>
2018-11-15 23:29:39 +0300

    -

---
+++ src/com/artur85/Game.java
@@ -0,0 +1,8 @@
+package com.artur85;
+
+public class Game {
+    public static void main(String[] args) {
+        UserInterface gui = new UserInterface();
+        gui.setVisible(true);
+    }
+}
--- src/com/artur85/UserInterface.java
+++ src/com/artur85/UserInterface.java
@@ -2,13 +2,32 @@ package com.artur85;
 
 import javax.swing.*;
 
-public class UserInterface {
+public class UserInterface extends JFrame {
     private JPanel mainPanel;
-    private JTextField sumDeposit;
+    private JTextField sumField;
+    private JTextField periodField;
     private JButton depositButton;
     private JList list1;
     private JButton buyButton;
-    private JButton button1;
     private JLabel businessLabel;
     private JLabel depositLabel;
+    private JLabel appName;
+    private JLabel wealth;
+    private JLabel wealthSize;
+    private JLabel earnings;
+    private JLabel earningsSize;
+    private JLabel depositInfo;
+    private JLabel depositPeriod;
+    private JLabel sumLabel;
+    private JLabel businessInfo;
+    private JSeparator topSeparator;
+
+
+    public UserInterface() {
+        super("My First App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(mainPanel);
+        setSize(600, 400);
+        //pack();
+    }
 }
