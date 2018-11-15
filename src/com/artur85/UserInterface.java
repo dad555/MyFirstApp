bad3eb3e019d0d6c8ca9ef3ac9d161cc13feaa35
Artur <artur@briginvest.ru>
2018-11-15 19:02:36 +0300

    Initial commit

---
+++ src/com/artur85/UserInterface.java
@@ -0,0 +1,14 @@
+package com.artur85;
+
+import javax.swing.*;
+
+public class UserInterface {
+    private JPanel mainPanel;
+    private JTextField sumDeposit;
+    private JButton depositButton;
+    private JList list1;
+    private JButton buyButton;
+    private JButton button1;
+    private JLabel businessLabel;
+    private JLabel depositLabel;
+}
