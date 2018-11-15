86e222383e9f1d10531735ed2e39ef111f05ec0e
Artur <artur@briginvest.ru>
2018-11-15 18:56:21 +0300

    Initial commit

---
+++ src/com/artur85/App.java
@@ -0,0 +1,12 @@
+package com.artur85;
+
+import javax.swing.*;
+
+public class App {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JButton внестиButton;
+    private JList list1;
+    private JButton купитьButton;
+    private JButton button1;
+}
